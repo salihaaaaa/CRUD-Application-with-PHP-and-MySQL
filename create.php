d2f6bd55bac70bf6734dec539d969fd6fea02dee
saliha <107999674+salihaaaaa@users.noreply.github.com>
2022-11-22 17:43:46 +0800

    Added create
    Create new records with an HTML form and send data to the server with a POST request method

---
+++ create.php
@@ -0,0 +1,48 @@
+<?php 
+include 'connectdb.php'; 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+    $result = $conn->query($sql);
+
+    if ($result) {
+        echo 'New record created successfully';
+        header('location: view.php');
+    } else {
+        echo 'Error ' .$sql . "<br>" .$conn->error;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+    <div class="container">
+        <h3>Add new users</h3>
+        <form method="post" action="create.php">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" name="name" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" id="email">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
